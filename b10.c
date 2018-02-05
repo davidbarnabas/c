22fef7abb842857f4d3f1dc0e70a3b372f1a98d7
davidbarnabas <35915198+davidbarnabas@users.noreply.github.com>
2018-02-05 14:01:31 +0530

    Update b10.c

--- b10.c
+++ b10.c
@@ -3,14 +3,14 @@
 void main()
 {
     int count=0;
-long long j;
-printf("Enter the number : ");
-scanf("%lld",&j);
-while(j!=0)
+    long long j;
+    printf("Enter the number : ");
+    scanf("%lld",&j);
+    while(j!=0)
 {
-count++;
-j=j/10;
+    count++;
+    j=j/10;
 }
-printf("The count of the number is %d",count);
-getch();
+    printf("The count of the number is %d",count);
+    getch();
 }
